67bee1350522a48eb1f0dbeb068b87e95fd706ba
Gabriel Meghnagi <gabrielmeghnagi@outlook.it>
2021-05-12 13:35:58 +0200

    Stable

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.6-alpine
-COPY . /prinquest
-WORKDIR /prinquest
 RUN pip install flask
 EXPOSE 8080
+COPY . /prinquest
+WORKDIR /prinquest
 ENTRYPOINT ["python"]
 CMD ["app/app.py"]
--- app/app.py
+++ app/app.py
@@ -1,8 +1,6 @@
 import os
 import json
 from flask import Flask, render_template, make_response, request
-
-
 app = Flask(__name__)
 
 
@@ -14,22 +12,23 @@ app.jinja_env.filters['tojson_pretty'] = to_pretty_json
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    pwd = os.getcwd()
     if request.method == 'POST':
         headerz = dict(request.headers)
-        with open('headers', 'w') as fp:
+        with open(f'{pwd}/app/headers', 'w') as fp:
          json.dump(headerz, fp)
         body = request.get_json()
-        with open('request', 'w') as fp:
+        with open(f'{pwd}/app/request', 'w') as fp:
             json.dump(body, fp)
         return ('', 204)
 
     if request.method == 'GET':
         output= ''
-        with open('headers', 'r') as fp:
+        with open(f'{pwd}/app/headers', 'r') as fp:
             headerz = json.load(fp)
         for k in headerz:
             output += f'{k} = {headerz[k]}\n'
-        with open('request', 'r') as fp:
+        with open(f'{pwd}/app/request', 'r') as fp:
             body = json.load(fp)
         resp = make_response(render_template("index.html", struct=output, body=body))
         resp.headers['Content-Type'] = 'text/plain'
